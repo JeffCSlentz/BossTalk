bbbcbd8c6b8cf5d1b3c0f272ead2436a6cf8ab63
JeffSlentz <JeffCSlentz@gmail.com>
2020-02-29 03:00:27 -0500

    fixed guildTags, AGAIN

--- commands/eval.js
+++ commands/eval.js
@@ -4,5 +4,6 @@ module.exports = {
     guildOnly: false,
     execute(message, args) {
       console.log(args.join(" "));
+      console.log(message.client.guildTags.get("248890462746836993"))
     },
 };
