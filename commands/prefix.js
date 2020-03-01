997d238ce446aa3cf0849656d41717cfe51e590d
JeffSlentz <JeffCSlentz@gmail.com>
2020-03-01 12:27:44 -0500

    removed eval.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -2,9 +2,10 @@ const dataManip = require('../utility/dataManipulation.js');
 
 module.exports = {
     name: 'prefix',
-    description: 'Change my prefix.',
+    description: 'Change my prefix. Only available to administrators.',
     guildOnly: true,
     authorOnly: false,
+    adminOnly: true,
     execute(message, args) {
         if(!args.length){
             return message.channel.send([`My current prefix is ${message.client.getPrefix(message)}`, `But you already knew that ;)`])
