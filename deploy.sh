4b9974965b48f98184d5f87217291400c178483e
JeffSlentz <JeffCSlentz@gmail.com>
2023-04-15 03:48:48 -0400

    backquotes are dangerous

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,5 @@ npm install
 echo "Updating Commands"
 node ./src/deploy_commands.js
 
-echo "Running BossTalk with pm2. SSH in and monitor with `pm2 log`"
+echo "Running BossTalk with pm2. SSH in and monitor with pm2 log"
 pm2 start ./index.js
